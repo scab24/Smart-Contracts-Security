944e64c6473f7f6e92012816947144d7f677fb97
scab24 <git.seco@protonmail.com>
2023-02-05 19:13:08 +0100

    security

--- echidna/Overflow/Token.sol
+++ echidna/Overflow/Token.sol
@@ -41,16 +41,3 @@ contract Token is Pausable{
         balances[to] += value;
     }
 }
-//====================================================================================
-contract TestToken is Token {
-    address echidna_caller = msg.sender;
-
-    constructor() public {
-        balances[echidna_caller] = 10000;
-    }
-
-    // add the property
-    function echidna_test_balance() public view returns (bool) {
-        return (balances[echidna_caller] <= 10000);
-    }
-}
