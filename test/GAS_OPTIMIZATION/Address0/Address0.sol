4ac2eb3f8ccd8ab5b20d640d16d15efcbcc8c122
scab24 <git.seco@protonmail.com>
2023-02-21 17:49:52 +0100

    security

---
+++ test/GAS_OPTIMIZATION/Address0/Address0.sol
@@ -0,0 +1,37 @@
+  // SPDX-License-Identifier: MIT
+pragma solidity ^0.7.6;
+
+import "lib/forge-std/src/Test.sol";
+
+contract GasTestAddress0 is Test {
+    Contract0 c0;
+    Contract1 c1;
+
+    function setUp() public {
+        c0 = new Contract0();
+        c1 = new Contract1();
+    }
+
+    function testGas() public view {
+        c0.ownerNotZero(address(this));
+        c1.assemblyOwnerNotZero(address(this));
+    }
+}
+
+contract Contract0 {
+    function ownerNotZero(address _addr) public pure {
+        require(_addr != address(0), "zero address)");
+    }
+}
+
+contract Contract1 {
+    function assemblyOwnerNotZero(address _addr) public pure {
+        assembly {
+            if iszero(_addr) {
+                mstore(0x00, "zero address")
+                revert(0x00, 0x20)
+            }
+        }
+    }
+}
+
---
+++ test/GAS_OPTIMIZATION/Write_Storage_Values/Storage.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.6;
+
+import "lib/forge-std/src/Test.sol";
+contract GasTestStorage is Test {
+    Contract0 c0;
+    Contract1 c1;
+
+    function setUp() public {
+        c0 = new Contract0();
+        c1 = new Contract1();
+    }
+
+    function testGas() public {
+        c0.updateOwner(0x158B28A1b1CB1BE12C6bD8f5a646a0e3B2024734);
+        c1.assemblyUpdateOwner(0x158B28A1b1CB1BE12C6bD8f5a646a0e3B2024734);
+    }
+}
+
+contract Contract0 {
+    address owner = 0xb4c79daB8f259C7Aee6E5b2Aa729821864227e84;
+
+    function updateOwner(address newOwner) public {
+        owner = newOwner;
+    }
+}
+
+contract Contract1 {
+    address owner = 0xb4c79daB8f259C7Aee6E5b2Aa729821864227e84;
+
+    function assemblyUpdateOwner(address newOwner) public {
+        assembly {
+            sstore(owner.slot, newOwner)
+        }
+    }
+}
